67472f5e54500fa388d0104f05cd894551ff3fc9
Veliga Syutkin <veliga99@gmail.com>
2026-06-28 16:45:41 +0200

    fix(scroll): last line not erased on scroll (fixed).

--- kernel/srcs/keyboard.c
+++
@@ -1,6 +0,0 @@
-#include "kernel.h"
-
-void	keyboard_handler(void)
-{
-	inb(0x60);
-}
--- kernel/srcs/vga.c
+++ kernel/srcs/vga.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vga.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: benpicar <benpicar@student.42mulhouse.fr > +#+  +:+       +#+        */
+/*   By: vsyutkin <vsyutkin@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 15:22:40 by benpicar          #+#    #+#             */
-/*   Updated: 2026/06/28 16:38:45 by benpicar         ###   ########.fr       */
+/*   Updated: 2026/06/28 16:43:39 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int		g_cur = 0;
 
 void	switch_screen(int screen)
 {
-	size_t i, j;
 	if (screen < 0 || screen > 1)
 		return;
 	g_cur = screen;
@@ -29,11 +28,11 @@ void	switch_screen(int screen)
 void	scroll()
 {
 	t_vga *vga_cur = &g_screens[g_cur];
-	size_t i, j;
 
 	ft_memmove(vga_cur->lines[0], vga_cur->lines[1], sizeof(vga_cur->lines)
 		- sizeof(vga_cur->lines[0]));
 	vga_cur->cursor_y--;
+	ft_bzero(vga_cur->lines[24], sizeof(vga_cur->lines[24]));
 	ft_memcpy(vga, g_screens[g_cur].lines, sizeof(g_screens[g_cur].lines));
 	update_cursor();
 }
